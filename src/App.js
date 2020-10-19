d852b08ca703d169b2a074d4bb3236db10c63a9c
DmanCoder <ktdenis.1@live.com>
2020-10-20 09:11:27 +1000

    Layout setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import Navigation from './components/navigation';
 import Banner from './components/banner';
 
-import img1 from './assets/img/pizza-3007395_1920.jpg';
+import img1 from './assets/img/pexels-ray-piedra-1565982.jpg';
 import img2 from './assets/img/pexels-photo-3952043.jpeg';
 import img3 from './assets/img/pexels-klayfe-rohden-873653.jpg';
 
@@ -55,7 +55,7 @@ function App() {
         content={
           <h1>
             <span></span>
-            <span>Italian Specialities</span>
+            <span>Our Specialities</span>
           </h1>
         }
         display="none"
@@ -111,7 +111,7 @@ function App() {
         content={
           <h1>
             <span></span>
-            <span>Our specialties</span>
+            <span></span>
           </h1>
         }
         display="none"
@@ -127,8 +127,9 @@ function App() {
             <div className="menu__content">
               <h6>Pizza Mexicana</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -138,8 +139,9 @@ function App() {
             <div className="menu__content">
               <h6>Pizza Mexicana</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -149,8 +151,9 @@ function App() {
             <div className="menu__content">
               <h6>Pizza Quatro Stagioni</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -160,8 +163,9 @@ function App() {
             <div className="menu__content">
               <h6>Pizza Quatro formagi</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -171,8 +175,9 @@ function App() {
             <div className="menu__content">
               <h6>Pizza Carnivora</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -182,8 +187,9 @@ function App() {
             <div className="menu__content">
               <h6>Lasagna</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -193,8 +199,9 @@ function App() {
             <div className="menu__content">
               <h6>Lasagna Napolitana</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
@@ -204,23 +211,26 @@ function App() {
             <div className="menu__content">
               <h6>Other lasagna</h6>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                odit possimus error...........................................
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem consequuntur nulla in amet consec,
+                laudantium........................................
               </p>
             </div>
 
             <span className="menu__price">$17</span>
           </div>
         </div>
-        <div className="menu">
+
+        <div className="menu menu-margin">
           <h5>Deserts</h5>
           <div className="menu__items">
             <div className="menu__item">
               <div className="menu__content">
                 <h6>Tiramsiu</h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                  odit possimus error...........................................
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem consequuntur nulla in amet consec,
+                  laudantium........................................
                 </p>
               </div>
 
@@ -230,8 +240,9 @@ function App() {
               <div className="menu__content">
                 <h6>Cannoli</h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                  odit possimus error...........................................
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem consequuntur nulla in amet consec,
+                  laudantium........................................
                 </p>
               </div>
 
@@ -241,8 +252,9 @@ function App() {
               <div className="menu__content">
                 <h6>Double Choco Biscotti</h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                  odit possimus error...........................................
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem consequuntur nulla in amet consec,
+                  laudantium........................................
                 </p>
               </div>
 
@@ -252,8 +264,9 @@ function App() {
               <div className="menu__content">
                 <h6>Sicilian Ricotta</h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                  odit possimus error...........................................
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem consequuntur nulla in amet consec,
+                  laudantium........................................
                 </p>
               </div>
 
@@ -263,8 +276,9 @@ function App() {
               <div className="menu__content">
                 <h6>Pizzelles</h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                  odit possimus error...........................................
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem consequuntur nulla in amet consec,
+                  laudantium........................................
                 </p>
               </div>
 
